08a1b78d196e7459694bb9ef2d360beba476ffd1
Pavel <pavel.buklemishev@gmail.com>
2024-11-06 23:29:45 -0800

    Update problem6d2.m
    updated 6d2 as I made a mistake in the matrix

--- hw4/problem6/problem6d2.m
+++ hw4/problem6/problem6d2.m
@@ -11,10 +11,10 @@ for i = 1:length(h_values)
     Dxx = spdiags([e (-2*e - h*h*e) e], [-1 0 1], m, m);
 
 
-    Dxx(1,1) = 1+h*h;
-    Dxx(m,m) = 1+h*h;
-    Dxx(1,2) =1;
-    Dxx(m, m-1)=1;
+    Dxx(1,1) = -(2+h*h);;
+    Dxx(m,m) = -(2+h*h);;
+    Dxx(1,2) =0;
+    Dxx(m, m-1)=0;
     %disp(Dxx)
     
     Dxx = Dxx / h^2;
